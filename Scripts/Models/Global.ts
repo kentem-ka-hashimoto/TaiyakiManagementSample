64d2f5976195c18c3c4148f7470ae9c24760c5ab
ka-hashimoto <ka-hashimoto@kentem.co.jp>
2022-07-08 08:52:39 +0900

    Globalクラスを作成

---
+++ Scripts/Models/Global.ts
@@ -0,0 +1,26 @@
+import { TaiyakiManager } from './taiyakiManager.js';
+import taiyakiKind from '../Types/kind.js';
+import Size from '../Types/size.js';
+import { Taiyaki } from './taiyaki.js';
+
+export class Global {
+  private static _taiyakiArrMg: TaiyakiManager;
+
+  public static get taiyakiArrMg() {
+    if (!this._taiyakiArrMg) {
+      this._taiyakiArrMg = new TaiyakiManager();
+    }
+    return this._taiyakiArrMg;
+  }
+
+  public static getLocalStorage(): void {
+    const items: string | null = localStorage.getItem('taiyakiDate');
+    if (items) {
+      const taiyakiArr: string[] = JSON.parse(items);
+      taiyakiArr.forEach((taiyaki: any) => {
+        const instance = this._taiyakiArrMg.createTaiyaki(taiyaki._kind, taiyaki._size);
+        this._taiyakiArrMg.add(instance);
+      });
+    }
+  }
+}
