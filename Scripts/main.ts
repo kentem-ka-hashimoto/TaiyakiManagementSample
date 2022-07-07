4e18560225526365e14337e5d5b4011a5a037af6
ka-hashimoto <ka-hashimoto@kentem.co.jp>
2022-07-07 13:28:43 +0900

    リストの作成を行う関数の側を実装

--- Scripts/main.ts
+++ Scripts/main.ts
@@ -1,5 +1,9 @@
+// tbodyの取得
+const tbody: HTMLTableSectionElement | null = document.querySelector('tbody');
 // 終了ボタンの取得
 const endBtn = document.getElementById('close') as HTMLButtonElement;
+
+createListView();
 
 // 終了ボタンの処理
 endBtn.addEventListener('click', () => {
@@ -8,3 +12,26 @@ endBtn.addEventListener('click', () => {
     alert('閉じるのに失敗しました。直接このタブを閉じて下さい。');
   }
 });
+
+// 項目リストの作成
+function createListView(): void {
+  const checkBox: HTMLInputElement = document.createElement('input');
+  checkBox.type = 'checkbox';
+  const tdOfCheck: HTMLTableCellElement = document.createElement('td');
+  tdOfCheck.id = 'check';
+  tdOfCheck.appendChild(checkBox);
+  const tdOfKind: HTMLTableCellElement = document.createElement('td');
+  tdOfKind.textContent = '通常たい焼き';
+  const tdOfContent: HTMLTableCellElement = document.createElement('td');
+  tdOfContent.textContent = 'あんこ';
+  const tdOfSize: HTMLTableCellElement = document.createElement('td');
+  tdOfSize.textContent = '小';
+  const tdOfPrice: HTMLTableCellElement = document.createElement('td');
+  tdOfPrice.textContent = '100';
+
+  tbody?.appendChild(tdOfCheck);
+  tbody?.appendChild(tdOfKind);
+  tbody?.appendChild(tdOfContent);
+  tbody?.appendChild(tdOfSize);
+  tbody?.appendChild(tdOfPrice);
+}
