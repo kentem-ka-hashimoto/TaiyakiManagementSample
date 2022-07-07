d07e65d55d9d36eb30b9cecbf65054a362c3aece
ka-hashimoto <ka-hashimoto@kentem.co.jp>
2022-07-07 16:06:53 +0900

    各ラジオボタンの要素の取得を実装

--- Scripts/Models/selection.ts
+++ Scripts/Models/selection.ts
@@ -3,12 +3,24 @@ const purchaseBtn = document.getElementById('purchase') as HTMLButtonElement;
 // キャンセルボタンの取得
 const cancelBtn = document.getElementById('cancel') as HTMLButtonElement;
 
-// 購入ボタンの取得
+// メニューラジオボタンの取得
+const menuBtns: NodeListOf<HTMLElement> = document.getElementsByName('kind');
+const usuBtn = menuBtns[0] as HTMLInputElement;
+const cusBtn = menuBtns[1] as HTMLInputElement;
+const dxBtn = menuBtns[2] as HTMLInputElement;
+
+// サイズラジオボタンの取得
+const sizeBtns: NodeListOf<HTMLElement> = document.getElementsByName('size');
+const lBtn = sizeBtns[0] as HTMLInputElement;
+const mBtn = sizeBtns[1] as HTMLInputElement;
+const sBtn = sizeBtns[2] as HTMLInputElement;
+
+// 購入ボタンの処理
 purchaseBtn.addEventListener('click', () => {
   window.location.href = 'main.html';
 });
 
-// キャンセルボタンの取得
+// キャンセルボタンの処理
 cancelBtn.addEventListener('click', () => {
   window.location.href = 'main.html';
 });
