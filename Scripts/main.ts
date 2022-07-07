3d32d7d7426db6628c6b4a801efa1574209bc8b5
ka-hashimoto <ka-hashimoto@kentem.co.jp>
2022-07-07 13:13:52 +0900

    終了ボタンの処理を実装

--- Scripts/main.ts
+++ Scripts/main.ts
@@ -1,1 +1,10 @@
+// 終了ボタンの取得
+const endBtn = document.getElementById('close') as HTMLButtonElement;
 
+// 終了ボタンの処理
+endBtn.addEventListener('click', () => {
+  window.close();
+  if (!window.closed) {
+    alert('閉じるのに失敗しました。直接このタブを閉じて下さい。');
+  }
+});
