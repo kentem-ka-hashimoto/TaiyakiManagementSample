fe8a8e0c6ea7a1e85b2abf5ae86b22d19212e048
ka-hashimoto <ka-hashimoto@kentem.co.jp>
2022-07-08 15:55:08 +0900

    ファイル名をadd.tsに変更

--- Scripts/Models/add.ts
+++ Scripts/Models/add.ts
@@ -2,7 +2,6 @@ import taiyakiKind from '../Types/kind.js';
 import Size from '../Types/size.js';
 import { Global } from './Global.js';
 import { Taiyaki } from './taiyaki.js';
-import { TaiyakiManager } from './taiyakiManager.js';
 
 // 購入ボタンの取得
 const purchaseBtn = document.getElementById('purchase') as HTMLButtonElement;
@@ -13,8 +12,6 @@ const role: string | null = localStorage.getItem('role');
 
 // メニューラジオボタンの取得
 const menuBtns: NodeListOf<HTMLElement> = document.getElementsByName('kind');
-
-for (let i = 0; i < menuBtns.length; i++) {}
 const usuBtn = menuBtns[0] as HTMLInputElement;
 const cusBtn = menuBtns[1] as HTMLInputElement;
 const dxBtn = menuBtns[2] as HTMLInputElement;
@@ -25,24 +22,19 @@ const lBtn = sizeBtns[0] as HTMLInputElement;
 const mBtn = sizeBtns[1] as HTMLInputElement;
 const sBtn = sizeBtns[2] as HTMLInputElement;
 
+// 追加処理
 if (role && role === 'add') {
   usuBtn.checked = true;
   lBtn.checked = true;
-  // 購入ボタンの処理
+  // 購入ボタン
   let taiyaki: Taiyaki;
   purchaseBtn.addEventListener('click', () => {
     checkDxSize();
     const size: Size | undefined = checkSize();
     if (size !== undefined) {
-      if (usuBtn.checked) {
-        taiyaki = Global.taiyakiArrMg.createTaiyaki(taiyakiKind.Usually, size);
-      }
-      if (cusBtn.checked) {
-        taiyaki = Global.taiyakiArrMg.createTaiyaki(taiyakiKind.Custard, size);
-      }
-      if (dxBtn.checked) {
-        taiyaki = Global.taiyakiArrMg.createTaiyaki(taiyakiKind.Deluxe, size);
-      }
+      if (usuBtn.checked) taiyaki = Global.taiyakiArrMg.createTaiyaki(taiyakiKind.Usually, size);
+      if (cusBtn.checked) taiyaki = Global.taiyakiArrMg.createTaiyaki(taiyakiKind.Custard, size);
+      if (dxBtn.checked) taiyaki = Global.taiyakiArrMg.createTaiyaki(taiyakiKind.Deluxe, size);
     }
     addLocalStorage(taiyaki);
     window.location.href = 'main.html';
@@ -54,18 +46,21 @@ cancelBtn.addEventListener('click', () => {
   window.location.href = 'main.html';
 });
 
+// サイズのチェックを行う関数
 export function checkSize(): Size | undefined {
   if (lBtn.checked) return Size.L;
   if (mBtn.checked) return Size.M;
   if (sBtn.checked) return Size.S;
 }
 
+// デラックスのサイズ確認とアラート表示
 export function checkDxSize(): void {
   if (dxBtn.checked && !lBtn.checked) {
     alert(`デラックスたい焼きは、'大'しか選択できません。`);
   }
 }
 
+// ローカルストレージへの保存
 function addLocalStorage(taiyaki: Taiyaki): void {
   Global.getLocalStorage();
   Global.taiyakiArrMg.add(taiyaki);
